4a7ba5593d6d17281edcc7d39a6f36d51c0cda33
MicroFish91 <mwfisher91@gmail.com>
2018-06-26 12:01:45 -0500

    6/26/18 HW

---
+++ python_exercises/exercise2/exercise2.py
@@ -0,0 +1,2 @@
+name = input("WHAT IS YOUR NAME? ")
+print("HELLO {upper_name}! YOUR NAME HAS {name_length} LETTERS IN IT! AWESOME!".format(upper_name = name.upper(), name_length = len(name)))
---
+++ python_exercises/exercise2/exercise3.py
@@ -0,0 +1,7 @@
+print("Please fill in the blanks below: ")
+print("__(name)__'s favorite activity at work is __(activity)__.")
+
+name = input("What is name? ")
+activity = input("What is activity? ")
+
+print(name + "'s favorite activity at work is " + activity + ".")
