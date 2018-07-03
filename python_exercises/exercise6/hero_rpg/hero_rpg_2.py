37cecf07171f59b8634972478930427a52d81eb2
MicroFish91 <mwfisher91@gmail.com>
2018-07-03 11:14:39 -0500

    Added special ability and attributes to RPG 2

--- python_exercises/exercise6/hero_rpg/hero_rpg_2.py
+++ python_exercises/exercise6/hero_rpg/hero_rpg_2.py
@@ -8,49 +8,104 @@
 
 import random
 
+# Array holds experience required for each level up
+levelUp = [100, 200, 400, 800, 1200, 1800, 2400, 3000, 5000, 8000, 12000, 18000, 25000]
+
 class Character:
 
-    def __init__(self, health, power, charType, special):
+    def __init__(self, health, power, charType, special, evasion, defense, critical):
         self.health = health
         self.power = power
         self.charType = charType
         self.special = special
+        self.evasion = evasion
+        self.defense = defense
+        self.critChance = critical
+        self.critMult = 2
+        self.level = 1
+        self.experience = 0
 
+    # Basic Attack
     def attack(self, foe):
         foe.health -= self.power
         print("{} does {} damage to the {}.".format(self.charType, self.power, foe.charType))
         if foe.health <= 0:
             print("The {} is dead.".format(foe.charType))
 
+    # Returns a float indicating final damage multiplier after critical chance and damage are taken into account
+    def criticalDamage(self):
+        if random.randint(0, 100) <= self.critChance:
+            print("WOW! CRITICAL HIT!! {}X damage multiplier activated!!!".format(self.critMult))
+            return self.critMult
+        else:
+            return 1
+
+    # Returns a boolean to see if attack lands
+    def attackLand(self):
+        if random.randint(0, 100) <= self.evasion:
+            return False
+        else:
+            return True
+
+    # Returns a boolean to see if character is still alive
     def alive(self):
         if self.health > 0:
             return True
         else:
             return False
 
+    # Prints key attributes of the character
     def print_status(self):
-        print("{} has {} health, {} power, and {} special.".format(self.charType, self.health, self.power, self.special))
+        print("{} is level {}. {} has {} health, {} power, {} special, {} evasion, and {} defense.".format(self.charType, self.level, self.charType self.health, self.power, self.special, self.evasion, self.defense))
 
-    
+    # When level up requirement is met, stat increase is triggered
+    def levelUp(self):
+        self.health += 2
+        self.power += 1
+        self.special += 2
+        self.evasion += 1
+        self.defense += 1
+        self.critChance += 1
+        self.level += 1
+
+# Hero is resilient and has the double damage special ability
+# At level 5 and above, automatically survives through one fatal blow
 class Hero(Character):
 
     def __init__(self):
-        super().__init__(15, 4, "Hero", "5")
+        super().__init__(15, 5, "Hero", 10, 5, 2, 2)
+        self.fatalBlow = 1
 
     def special_ability(self, foe):
-
-
+        if self.special_ability >= 5:
+            foe.health -= (self.power * 2)
+            self.special_ability -= 5
+            print("{} does {} damage to the {}.".format(self.charType, self.power * 2, foe.charType))
+            print("{} has {} SP remaining.".format(self.charType, self.special_ability))
+        else:
+            print("{} winds up to perform a devastating blow.. wait, {} totally whiffed.".format(self.charType, self.charType))
+        if foe.health <= 0:
+            print("The {} is dead.".format(foe.charType))
         
 
 class Goblin(Character):
-    
+
     def __init__(self):
-        super().__init__(10, 2, "Goblin")
+        super().__init__(20, 3, "Goblin", 5, 1, 1, 5)
+        self.experience = 50
+
+    def special_ability(self, foe):
+        print("Nothing happened.")
+        
 
 
 class Zombie(Character):
     def __init__(self):
-        super().__init__(30, 1, "Zombie")
+        super().__init__(100, 1, "Zombie", 0, 0, 0, 0)
+        self.experience = 200
+
+    def special_ability(self, foe):
+        print("Nothing happened.")
 
 
 def chooseProtagonist():
@@ -119,7 +174,6 @@ def chooseFoe():
 
     return myFoe
 
-
 def main():
 
     # Initialize variables
@@ -135,23 +189,62 @@ def main():
         print("1. Basic Attack ")
         print("2. Special Ability (Cost: 5) ")
         print("3. Evasive Maneuver ")
+        print("4. Use Item")
+        print("5. Flee")
         print("> ", end=' ')
 
-        # raw_input = input()
-        # if raw_input == "1":
-        #     # Hero attacks goblin
-        #     myHero.attack(myGoblin)
-        # elif raw_input == "2":
-        #     pass
-        # elif raw_input == "3":
-        #     print("Goodbye.")
-        #     break
-        # else:
-        #     print("Invalid input {}".format(raw_input))
+        userInput = input()
 
-        # if myGoblin.health > 0:
-        #     # Goblin attacks hero
-        #     myGoblin.attack(myHero)
+        # ************************
+        # ***   Player Move    ***
+        # ************************
+
+        # Basic Attack
+        if userInput == "1":
+            myHero.attack(myFoe)
+
+        # Special Attack
+        elif userInput == "2":
+            myHero.special_ability(myFoe)
+        
+        # Evasive Maneuver
+        elif userInput == "3":
+            
+        # Use Item
+        elif userInput == "4":
+
+        # Flee
+        elif userInput == "5":
+
+        
+        # Invalid output
+        else:
+            print("Invalid input.")
+            print("Your character's indecision cost you your turn.")
+
+
+
+        # ************************
+        # ***    Enemy Move    ***
+        # ************************
+
+        foeMove = random.randint(1, 2)
+
+        # Foe performs a basic attack
+        if foeMove == 1:
+            myFoe.attack(myHero)
+        
+        # Foe performs a special attack
+        elif foeMove == 2:
+            myFoe.special_ability(myHero)
+
+        # Foe performs an evasive maneuver
+        elif foeMove == 3:
+            
+        
+        # Shouldn't ever trigger
+        else:
+            print("That's strange, your foe chose to do nothing.")
 
 main()
 
