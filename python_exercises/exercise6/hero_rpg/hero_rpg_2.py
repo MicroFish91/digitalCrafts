38627abbc38a5438f9476fb5518e3de9e7eaf5e1
MicroFish91 <mwfisher91@gmail.com>
2018-07-03 14:36:13 -0500

    RPG 2: 7/3/18

--- python_exercises/exercise6/hero_rpg/hero_rpg_2.py
+++ python_exercises/exercise6/hero_rpg/hero_rpg_2.py
@@ -7,13 +7,15 @@
 # 3. flee
 
 import random
+import time
 
 # Array holds experience required for each level up
-levelUp = [100, 200, 400, 800, 1200, 1800, 2400, 3000, 5000, 8000, 12000, 18000, 25000]
+levelUp = [100, 200, 400, 800, 1200, 1800, 2400, 3000, 5000, 8000, 12000, 18000, 25000, 40000, 75000]
 
+# Parent class for all character models
 class Character:
 
-    def __init__(self, health, power, charType, special, evasion, defense, critical):
+    def __init__(self, health, power, charType, special, evasion, defense, critical, experience):
         self.health = health
         self.power = power
         self.charType = charType
@@ -23,12 +25,14 @@ class Character:
         self.critChance = critical
         self.critMult = 2
         self.level = 1
-        self.experience = 0
+        self.experience = experience
+        self.charging = False
 
     # Basic Attack
     def attack(self, foe):
-        foe.health -= self.power
-        print("{} does {} damage to the {}.".format(self.charType, self.power, foe.charType))
+        if self.attackLand:
+            foe.health -= (self.power - foe.defense)
+            print("{} does {} damage to the {}.".format(self.charType, self.power - foe.defense, foe.charType))
         if foe.health <= 0:
             print("The {} is dead.".format(foe.charType))
 
@@ -56,57 +60,134 @@ class Character:
 
     # Prints key attributes of the character
     def print_status(self):
-        print("{} is level {}. {} has {} health, {} power, {} special, {} evasion, and {} defense.".format(self.charType, self.level, self.charType self.health, self.power, self.special, self.evasion, self.defense))
+        print("{} is level {}. {} has {} health, {} power, {} special, {} evasion, and {} defense.".format(self.charType, self.level, self.charType, self.health, self.power, self.special, self.evasion, self.defense))
 
     # When level up requirement is met, stat increase is triggered
-    def levelUp(self):
-        self.health += 2
-        self.power += 1
-        self.special += 2
-        self.evasion += 1
-        self.defense += 1
-        self.critChance += 1
-        self.level += 1
+    def level_up(self, experience):
+        self.experience += experience
+
+        # Level up triggered!
+        while self.experience >= levelUp[self.level - 1]:
+            self.health += 2
+            self.power += 1
+            self.special += 2
+            self.evasion += 1
+            self.defense += 1
+            self.critChance += 1
+            self.experience -= levelUp[self.level - 1]
+            self.level += 1
+
+            print()
+            print("Congratulations again!! You have reached level {}. You gain the following stat points: ".format(self.level))
+            print("Health: {} + 2 = {}".format(self.health - 2, self.health))
+            print("Power: {} + 1 = {}".format(self.power - 1, self.power))
+            print("Special: {} + 2 = {}".format(self.special - 2, self.special))
+            print("Evasion: {} + 1 = {}".format(self.evasion - 1, self.evasion))
+            print("Defense: {} + 1 = {}".format(self.defense - 1, self.defense))
+            print("Crit. Chance: {} + 1 = {}".format(self.critMult - 1, self.critMult))
+
+        print()
+        print("You have {} XP remaining until you reach level {}!".format(levelUp[self.level] - self.experience, self.level + 1))
+
+    # Increases chance to dodge by 35%. Upon successful dodge, has a chance to trigger a counter
+    def evasive_maneuver(self, foe):
+        # Checks if the evasion condition is triggered
+        if random.randint(0, 100) >= (self.evasion + 35):
+            print("{} successfully evades {}.".format(self.charType, foe.charType))
+
+            # Counter triggered
+            if random.randint(0, 100) >= 50:
+                print("{} finds an opening and counters {} for {} damage!".format(self.charType, foe.charType, self.power - foe.defense))
+
+            else:
+                print("{} could not find an opening to perform a counter attack.".format(self.charType))
+        
+        else:
+        # Evade failed
+            self.health -= (foe.power - self.defense)
+            print("{} attemps to evade. It fails.".format(self.charType))
+            print("{} is hit by {} for {} damage.".format(self.charType, foe.charType, foe.power - self.defense))
 
 # Hero is resilient and has the double damage special ability
-# At level 5 and above, automatically survives through one fatal blow
 class Hero(Character):
 
     def __init__(self):
-        super().__init__(15, 5, "Hero", 10, 5, 2, 2)
+        super().__init__(15, 5, "Hero", 10, 5, 2, 2, 0)
         self.fatalBlow = 1
 
     def special_ability(self, foe):
-        if self.special_ability >= 5:
-            foe.health -= (self.power * 2)
-            self.special_ability -= 5
-            print("{} does {} damage to the {}.".format(self.charType, self.power * 2, foe.charType))
-            print("{} has {} SP remaining.".format(self.charType, self.special_ability))
+        # Executes if player has enough special ability points
+        if self.special >= 5:
+            # Executes if attack is not evaded
+            if self.attackLand:
+                foe.health -= ((self.power * 2) - foe.defense)
+                self.special -= 5
+                print("HYAAHHH!! SPECIAL ATTACK! {} does {} damage to the {}.".format(self.charType, self.power * 2, foe.charType))
+                print("{} has {} SP remaining.".format(self.charType, self.special))
+
+            else:
+            # If attack is evaded
+                self.special -= 5
+                print("{} misses {}.".format(self.charType, foe.charType))
+                print("{} has {} SP remaining.".format(self.charType, self.special))
+
         else:
-            print("{} winds up to perform a devastating blow.. wait, {} totally whiffed.".format(self.charType, self.charType))
+        # If not enough special ability points
+            print("{} winds up to perform a devastating blow.. wait... {} totally whiffed!".format(self.charType, self.charType))
+
         if foe.health <= 0:
             print("The {} is dead.".format(foe.charType))
         
-
 class Goblin(Character):
 
     def __init__(self):
-        super().__init__(20, 3, "Goblin", 5, 1, 1, 5)
-        self.experience = 50
+        super().__init__(20, 3, "Goblin", 5, 1, 1, 5, 50)
+
+    def attack(self, foe):
+        if self.charging:
+            _charging(foe)
+
+        elif self.attackLand:
+            foe.health -= (self.power - foe.defense)
+            print("{} does {} damage to the {}.".format(self.charType, self.power - foe.defense, foe.charType))
+
+        if foe.health <= 0:
+            print("The {} is dead.".format(foe.charType))
 
     def special_ability(self, foe):
-        print("Nothing happened.")
-        
+        if self.special >= 5 and not self.charging:   
+            # Performs special ability (charge) 
+            print("{} begins flailing about rapidly. It appears he is charging up for an attack.".format(self.charType))
+            self.special -= 5
+            self.charging = True
 
+        elif self.special < 5 and not self.charging:
+            # If not enough special ability points
+            print("{} winds up to perform a devastating blow.. wait... {} totally whiffed!".format(self.charType, self.charType))
 
+        elif self.charging:
+            _charging(foe)
+            
+
+    def _charging(self, foe):
+        if self.attackLand:
+            # Unleash the charged attack
+            foe.health -= (self.power * 3 - foe.defense)
+            print("{} unleashes his charged attack all over the {} for {} damage.".format(self.charType, foe.charType, self.power * 3 - foe.defense))
+            self.charging = False
+        else:
+            # Charged attack misses
+            print("{} misses his charge attack and runs into a wall. {} takes 2 damage.".format(self.charType, self.charType))
+            self.health -= 2
+            self.charging = False
+ 
 class Zombie(Character):
+
     def __init__(self):
-        super().__init__(100, 1, "Zombie", 0, 0, 0, 0)
-        self.experience = 200
+        super().__init__(100, 1, "Zombie", 0, 0, 0, 0, 200)
 
     def special_ability(self, foe):
         print("Nothing happened.")
-
 
 def chooseProtagonist():
     inputCheck = True
@@ -174,77 +255,111 @@ def chooseFoe():
 
     return myFoe
 
+def battlePrompt():
+    print("What do you want to do? ")
+    print("1. Basic Attack ")
+    print("2. Special Ability (Cost: 5) ")
+    print("3. Evasive Maneuver ")
+    print("4. Use Item")
+    print("5. Flee")
+    print("> ", end=' ')
+
+# Need to add max health function
+
+
 def main():
 
     # Initialize variables
+    playAgain = true
     myHero = chooseProtagonist()
     myFoe = chooseFoe()
 
-    while myHero.alive() and myFoe.alive():
-        myHero.print_status()
-        myFoe.print_status()       
-        print()
+    while playAgain:
+        # Initialize new foe
+        myFoe = chooseFoe()
 
-        print("What do you want to do? ")
-        print("1. Basic Attack ")
-        print("2. Special Ability (Cost: 5) ")
-        print("3. Evasive Maneuver ")
-        print("4. Use Item")
-        print("5. Flee")
-        print("> ", end=' ')
+        while myHero.alive() and myFoe.alive():
+            print()
+            myHero.print_status()
+            myFoe.print_status()       
+            print()
+            time.sleep(3)
 
-        userInput = input()
+            # Displays battle prompt options
+            battlePrompt()
 
-        # ************************
-        # ***   Player Move    ***
-        # ************************
-
-        # Basic Attack
-        if userInput == "1":
-            myHero.attack(myFoe)
-
-        # Special Attack
-        elif userInput == "2":
-            myHero.special_ability(myFoe)
-        
-        # Evasive Maneuver
-        elif userInput == "3":
+            userInput = input()
             
-        # Use Item
-        elif userInput == "4":
+            # ************************
+            # ***   Player Move    ***
+            # ************************
 
-        # Flee
-        elif userInput == "5":
+            # Basic Attack
+            if userInput == "1":
+                myHero.attack(myFoe)
 
-        
-        # Invalid output
-        else:
-            print("Invalid input.")
-            print("Your character's indecision cost you your turn.")
-
-
-
-        # ************************
-        # ***    Enemy Move    ***
-        # ************************
-
-        foeMove = random.randint(1, 2)
-
-        # Foe performs a basic attack
-        if foeMove == 1:
-            myFoe.attack(myHero)
-        
-        # Foe performs a special attack
-        elif foeMove == 2:
-            myFoe.special_ability(myHero)
-
-        # Foe performs an evasive maneuver
-        elif foeMove == 3:
+            # Special Attack
+            elif userInput == "2":
+                myHero.special_ability(myFoe)
             
-        
-        # Shouldn't ever trigger
-        else:
-            print("That's strange, your foe chose to do nothing.")
+            # Evasive Maneuver
+            elif userInput == "3":
+                print("You perform an evasive maneuver.")
+
+            # Use Item
+            elif userInput == "4":
+                print("You use an item.")
+
+            # Flee
+            elif userInput == "5":
+                print("You attempt to flee.")
+            
+            # Invalid output
+            else:
+                print("Invalid input.")
+                print("Your character's indecision cost you your turn.")
+
+            time.sleep(3)
+            print()
+
+            # ************************
+            # ***    Enemy Move    ***
+            # ************************
+
+            foeMove = random.randint(1, 2)
+
+            # If Foe is still alive
+            if myFoe.health > 0:
+
+                # Foe performs a basic attack
+                if foeMove == 1:
+                    print("{} retaliates with a basic attack!".format(myFoe.charType))
+                    time.sleep(3)
+                    myFoe.attack(myHero)
+                
+                # Foe performs a special attack
+                elif foeMove == 2:
+                    print("{} retaliates with a special attack!".format(myFoe.charType))
+                    time.sleep(3)
+                    myFoe.special_ability(myHero)
+                
+                # Foe uses an item
+                elif foeMove == 3:
+                    print("Foe uses an item.")
+                    time.sleep(3)
+
+                # Shouldn't ever trigger
+                else:
+                    print("That's strange, your foe chose to do nothing.")
+
+            else:
+            # If Foe is dead
+                print("Congratulations! {}'s death has granted you {} XP!!".format(myFoe.charType, myFoe.experience))
+                time.sleep(2)
+                myHero.level_up(myFoe.experience)
+                time.sleep(5)
+
+        userInput
 
 main()
 
